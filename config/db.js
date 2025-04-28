ebf44d53717abdf3383647cc83fe8c1799209182
smohit24 <mohit.n1724@gmail.com>
2025-04-28 10:23:17 +0530

    Web Application

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Connected to Mongodb database ${mongoose.connection.host}`);
+    } catch (error) {
+        console.log(`Mongodb Databse Error${error}`.bgRed.white);
+    }
+};
+
+module.exports = connectDB;
